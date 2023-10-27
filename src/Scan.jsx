f1d273320a44cd4c6fd7d1558f033d2a99454977
OthmaneAGHBAL <aghbal.othmane9@gmail.com>
2023-10-27 04:51:24 +0200

    add upload image to scan

--- src/Scan.jsx
+++ src/Scan.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import QrReader from 'react-qr-scanner'
-import {Button } from 'reactstrap'
+import QrScanner from 'qr-scanner'
+import {Button, Form,FormGroup,Input,FormText,Label } from 'reactstrap'
 
-import {MdOutlineCameraswitch} from 'react-icons/md'
+import {BsFillCloudUploadFill} from 'react-icons/bs'
 
 const Scan = () => {
 
   const [delay,setDelay] = useState(100)
   const [result,setResult] = useState('No result')
-  const [cameramode,setCameraMode] = useState('rear')
   const [isFound,setIsFound] = useState(false)
+  const [CameraOrFile,setCamerOrFile] = useState(false)
 
   const previewStyle = {
     width: '100%',
@@ -23,38 +24,40 @@ const Scan = () => {
       setIsFound(true)
     }
 
-
   }
   const handleError = (err) =>{
     console.error(err)
   }
-
-
-  const toggleCameraMode = () => {
-    if(cameramode === 'front'){
-      setCameraMode('rear');
-    }else{
-      setCameraMode('front');
-    }
-  };
 
   const HandleRescan = ()=>{
     setIsFound(false)
     setResult('')
   }
 
+  const readCode = (e)=>{
+    const file = e.target.files[0];
+        if (!file) {
+            return;
+        }
+        QrScanner.scanImage(file, { returnDetailedScanResult: true })
+            .then(result => {
+              setResult(result.data)
+            })
+            .catch(e => setResult(e));
+        setIsFound(true)
+  }
+
 
   return (
     <div >
         <div className=''>
-        {!isFound && (
+        {
+        !isFound && CameraOrFile && (
           <div className='text-center'>
-            <div className=' mb-4'><Button className='btn btn-primary'  onClick={toggleCameraMode}><MdOutlineCameraswitch className=' fs-4' /></Button ></div>
             <QrReader
             className="my-2"
             delay={delay}
             style={previewStyle}
-            facingMode={cameramode}
             onError={handleError}
             onScan={handleScan}
             />
@@ -70,7 +73,31 @@ const Scan = () => {
               <Button  onClick={HandleRescan} color="primary" outline >Rescan</Button>
             </div>
           )
-        }
+      }
+      {!isFound && !CameraOrFile && (
+        <Form>
+          <FormGroup className='image-upload-wrap border-3' >
+          <Input
+            name="file"
+            type="file"
+            onChange={readCode}
+            className='form-control file-upload-input'
+          />
+          <FormText className=' p-3 px-2 d-flex flex-column gap-2 align-items-center flex-lg-row fs-5'>
+            <BsFillCloudUploadFill className=' fs-4 text-black' />
+            <span className=' text-center text-black'>Upload an image that contains a QR code.</span>
+          </FormText>
+        </FormGroup>
+        </Form>
+      )  
+      }
+      {
+        !isFound && (
+        <div className=' text-center'>
+          <Button color='primary' outline onClick={()=>{setCamerOrFile(!CameraOrFile)}} >Switch(Camera/File)</Button>
+        </div>
+        )
+      }
       </div>
   )
 }
